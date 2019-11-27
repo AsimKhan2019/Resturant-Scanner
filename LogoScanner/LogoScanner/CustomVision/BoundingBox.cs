4406b8d9a3f1fab04f7004d436129c6f59e8bcfd
Lucia Cangarova <2330954c@student.gla.ac.uk>
2019-11-27 13:52:33 +0000

    feat Image Recognition: Implemented Image recognition
    Image recognition using Custom Vision API calls
    
    closes #50 fix TensorFlow build error on Android

---
+++ LogoScanner/LogoScanner/CustomVision/BoundingBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogoScanner
+{
+    public class BoundingBox
+    {
+        public double Left { get; set; }
+
+        public double Top { get; set; }
+
+        public double Width { get; set; }
+
+        public double Height { get; set; }
+    }
+}

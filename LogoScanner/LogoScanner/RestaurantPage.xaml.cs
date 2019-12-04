4b4f2cb749d3505f5b51dcbae2d11c44306578f1
Patrick Devanney <2329979d@student.gla.ac.uk>
2019-12-04 11:56:38 +0000

    feat(restaurant page): added a new API call
    Added an API call to the ConsumerAPI. This call returns the current promotions for the current restaurant. A function was created that allows the API call to be made more modular. The correct values display in a ListView on the MainPage.xaml.
    
    Close #64.

--- LogoScanner/LogoScanner/RestaurantPage.xaml.cs
+++ LogoScanner/LogoScanner/RestaurantPage.xaml.cs
@@ -1,23 +1,25 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
-using Newtonsoft.Json.Linq;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Internals;
 using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
-
 //Additional API Calls Required
-//Promotions - GetPromotions API Call
-//Get Availability for Date Range
+//Get Availability for Date Range - POST Request
 
 namespace LogoScanner
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RestaurantPage : TabbedPage
     {
+        private ObservableCollection<Promotions> promotions = new ObservableCollection<Promotions>();
+        public ObservableCollection<Promotions> Employees { get { return promotions; } }
+
         private string micrositename;
 
         public RestaurantPage(string micrositename)
@@ -39,12 +41,15 @@ namespace LogoScanner
                     case 0:
                         HomeTab.IconImageSource = "HomeIconFilled.png";
                         break;
+
                     case 1:
                         MenuTab.IconImageSource = "MenuIconFilled.png";
                         break;
+
                     case 2:
                         ReviewsTab.IconImageSource = "ReviewIconFilled.png";
                         break;
+
                     case 3:
                         ScanTab.IconImageSource = "ScanIconFilled.png";
                         Navigation.PushModalAsync(new MainPage());
@@ -81,7 +86,8 @@ namespace LogoScanner
 
         private async void GetRestaurantData(string url, string token)
         {
-            JObject result = await Requests.APICallGet(url, token);
+            JArray r = await Requests.APICallGet(url, token);
+            JObject result = (JObject)r.First;
 
             //Parse the API Call and split the JSon object into the various variables.
             Logo.Source = GetRestaurantField(result, "LogoUrl");
@@ -92,13 +98,40 @@ namespace LogoScanner
             if (result["PricePoint"].Type != JTokenType.Null) price = Int32.Parse(result["PricePoint"].ToString());
             PriceLabel.Text = GetRestaurantField(result, "PricePoint", "£", price);
 
-            int stars = (int) Math.Round(Double.Parse(result["AverageReviewScore"].ToString()), 0, MidpointRounding.AwayFromZero);
+            int stars = (int)Math.Round(Double.Parse(result["AverageReviewScore"].ToString()), 0, MidpointRounding.AwayFromZero);
             StarLabel.Text = GetRestaurantField(result, "AverageReviewScore", "★", stars);
-
 
             double latitude = Convert.ToDouble(result["Latitude"].ToString());
             double longitude = Convert.ToDouble(result["Longitude"].ToString());
             string name = result["Name"].ToString();
+
+            string[] promotion_ids = GetPromotionIDs(result);
+            PromotionsView.ItemsSource = promotions;
+
+            if (promotion_ids.Length > 0)
+            {
+                string promotions_url = "https://api.rdbranch.com/api/ConsumerApi/v1/Restaurant/" + this.micrositename + "/Promotion?";
+                StringBuilder builder = new StringBuilder();
+
+                builder.Append(promotions_url);
+                foreach (string id in promotion_ids)
+                {
+                    builder.Append("&promotionIds=" + id);
+                }
+
+                string p = builder.ToString();
+
+                JArray array_promotions = await Requests.APICallGet(builder.ToString(), token);
+
+                foreach (JToken pr in array_promotions)
+                {
+                    promotions.Add(new Promotions { Name = pr["Name"].ToString(), Description = pr["Description"].ToString() });
+                }
+            }
+            else
+            {
+                promotions.Add(new Promotions { Name = "No Promotions Currently Available!" });
+            }
 
             var pin = new Pin()
             {
@@ -124,24 +157,22 @@ namespace LogoScanner
         {
             if (json[field].Type == JTokenType.Null || string.IsNullOrEmpty(json[field].ToString()))
                 return "No Set " + field;
+            else if (json[field] is JArray)
+            {
+                StringBuilder builder = new StringBuilder();
+                JToken[] cuisines = json[field].ToArray();
+
+                foreach (string cuisine in cuisines)
+                {
+                    builder.Append(cuisine);
+                    if (cuisines.IndexOf(cuisine) != cuisines.Count() - 1) builder.Append(", ");
+                }
+
+                return builder.ToString();
+            }
             else
             {
-                if (json[field] is JArray)
-                {
-                    StringBuilder builder = new StringBuilder();
-                    JToken[] cuisines = json[field].ToArray();
-
-                    foreach (string cuisine in cuisines)
-                    {
-                        builder.Append(cuisine);
-                        if (cuisines.IndexOf(cuisine) != cuisines.Count() - 1) builder.Append(", ");
-                    }
-                    return builder.ToString();
-                }
-                else
-                {
-                    return json[field].ToString();
-                }
+                return json[field].ToString();
             }
         }
 
@@ -152,6 +183,33 @@ namespace LogoScanner
                 return "No Set " + field;
             else
                 return String.Concat(Enumerable.Repeat(symbol, i));
+        }
+
+        private string[] GetPromotionIDs(JObject json)
+        {
+            string[] returnvalue;
+
+            if (json["AvailablePromotions"].Type == JTokenType.Null || string.IsNullOrEmpty(json["AvailablePromotions"].ToString()))
+            {
+                returnvalue = new string[0];
+                returnvalue[0] = "No Promotions Currently Available";
+                return returnvalue;
+            }
+            else
+            {
+                JToken[] promotion_ids = json["AvailablePromotions"].ToArray();
+
+                int i = 0;
+                returnvalue = new string[promotion_ids.Length];
+
+                foreach (string id in promotion_ids)
+                {
+                    returnvalue[i] = id;
+                    i++;
+                }
+
+                return returnvalue;
+            }
         }
     }
 }

5719551383674797c023acc62fc0bc3d8fd4c6d8
Patrick Devanney <2329979d@student.gla.ac.uk>
2020-01-28 14:36:09 +0000

    (feat) Booking Feature
    Added a new Booking class. Waiting for confirmation from the client regarding how to implement it properly.
    
    Currently doesn't run properly. Will fix in future edits.

---
+++ LogoScanner/LogoScanner/Booking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Logoscanner
+{
+    public class Booking
+    {
+        public Booking()
+        {
+            public void makebooking(string microsite, string booking, int partysize)
+            {
+                string bookingdate = "";
+                string bookingtime = "";
+                booking = "https://www.resdiary.com/Restaurant/" + microsite +
+                            "/Book/Customer?bookingDateTime=" + bookingdate +
+                            "2020-01-29" +
+                            "T" + bookingtime.Substring(0, 2) +
+                            "19%3A00%3A00&covers=2";
+            https://www.resdiary.com/Restaurant/thepalais/Book/Customer?bookingDateTime=2020-01-28T15%3A30%3A00&covers=2
+            }
+        }
+    }
+}

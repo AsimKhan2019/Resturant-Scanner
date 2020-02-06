f97a27ecc2799aa5dc07d9ce64c3161faa68cdf4
olliegardner <olliegardner@gmail.com>
2020-02-06 22:46:33 +0000

    fix(LogoScanner.sln): merge conflict
    fixed merge conflict in LogoScanner.sln which was preventing the project loading in visual studio

--- LogoScanner/LogoScanner/RestaurantPage.xaml.cs
+++ LogoScanner/LogoScanner/RestaurantPage.xaml.cs
@@ -3,7 +3,6 @@ using LogoScanner.Helpers;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using Xamarin.Essentials;
